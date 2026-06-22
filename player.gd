e14ab61051ab14263e0f7803f177570c5da29313
beta. V <55759681+betaV0123456789@users.noreply.github.com>
2026-06-22 23:56:53 +0300

    Add files via upload
    i fixded gaem

--- player.gd
+++ player.gd
@@ -3,6 +3,7 @@ extends RigidBody3D
 var mouse_sensitivity := 0.001
 var twist_input := 0.0
 var pitch_input := 0.0
+var move_speed := 1200.0
 
 @onready var twist_pivot := $TwistPivot
 @onready var pitch_pivot := $TwistPivot/PitchPivot
@@ -19,14 +20,20 @@ func _process(delta: float) -> void:
 	input.x = Input.get_axis("straif_left", "straif_right")
 	input.z = Input.get_axis("move_forward", "move_backward")
 	
-	apply_central_force(input * 1200.0 * delta)
+	var forward = -twist_pivot.global_transform.basis.z
+	var right = twist_pivot.global_transform.basis.x
+	
+	var move_dir : Vector3 = (forward * -input.z + right * input.x).normalized()
+	
+	if move_dir != Vector3.ZERO:
+		apply_central_force(move_dir * move_speed * delta)
 	
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		
 	twist_pivot.rotate_y(twist_input)
 	pitch_pivot.rotate_x(pitch_input)
-	pitch_pivot.rotation.x = clamp(pitch_pivot.rotation.x, -0.5, 0.5)
+	pitch_pivot.rotation.x = clamp(pitch_pivot.rotation.x, -1.5, 1.5)
 	twist_input = 0.0
 	pitch_input = 0.0
 	
