963d2ae4cbb4636e865b5ab5c141ecf8a0a166a5
Mak4861 <hlendzian@outlook.de>
2026-06-24 12:18:47 +0200

    Update
    one bugfix for the dashing and physics adjustments

--- player.gd
+++ player.gd
@@ -5,10 +5,11 @@ var twist_input := 0.0
 var pitch_input := 0.0
 var move_speed := 2400.0
 var dashDir = Vector3()
-var DashTime = 0.4
-var DashSpeed = 7500
+var DashTime = 0.3
+var DashSpeed = 4850
 var DashesLeft = 2
 var  IsDashing = false
+var HasGottenSlowed = false #variable to make shure that the movement speed nerf is only applied once when dashing
 var CanDash = true
 @onready var twist_pivot := $TwistPivot
 @onready var pitch_pivot := $TwistPivot/PitchPivot
@@ -28,15 +29,17 @@ func _process(delta: float) -> void:
 		input.x = Input.get_axis("straif_left", "straif_right")
 
 		input.z = Input.get_axis("move_forward", "Move_Back")
-	var HasGottenSlowed = false #variable to make shure that the movement speed nerf is only applied once when dashing
+	
 	#make the player slower while dashing so they dont have as much control
 	if(IsDashing):
-		
 		if(!HasGottenSlowed):
-			move_speed = move_speed / 2.5
+			HasGottenSlowed = true
+			move_speed = move_speed / 5
 	else:
 		if(HasGottenSlowed):
-			move_speed = move_speed * 2.5
+			HasGottenSlowed = false
+			await get_tree().create_timer(0.2).timeout #keep the player slowed for a short while so the dash needs to be used more strategicly
+			move_speed = move_speed * 5
 	var forward = -twist_pivot.global_transform.basis.z
 	var right = twist_pivot.global_transform.basis.x
 	var move_dir : Vector3 = (forward * -input.z + right * input.x).normalized()
@@ -64,12 +67,15 @@ func _process(delta: float) -> void:
 	
 	
 func DashCooldown():
+	var current_velocity = linear_velocity
 	CanDash =false
 	DashesLeft -= 1
 	IsDashing = true
 	await get_tree().create_timer(DashTime).timeout
+	current_velocity.x = 0 #stop sliding after dash
+	current_velocity.z = 0
 	IsDashing = false
-	await get_tree().create_timer(0.3).timeout
+	await get_tree().create_timer(3).timeout
 	DashesLeft += 1
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
